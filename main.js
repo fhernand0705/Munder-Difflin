dc0c572ec0d80716ae53c824f532b0adc4273c95
Freddy Hernandez <freddy.hernandez@fandm.edu>
2018-10-30 20:03:24 -0400

    more styling for the blocks and included JS eventcall back

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 var block = document.getElementsByClassName('block');
 
-
-function shadowIn() {
-  block.classList.toggle('hidden', false);
-  console.log(block);
+function viewSale(h1) {
+  h1.innerHTML = 'ENJOY 15% OFF YOUR FIRST ORDER!';
 }
-
-function shadowOut() {
-  block.classList.toggle('hidden', true);
-  console.log(block);
-}
-
-block.addEventListener('mouseover', shadowIn);
-block.addEventListener('mouseout', shadowOut);
