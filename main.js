6243dbf93afc155fe6e770f6403a17931cb1bae0
Freddy Hernandez <freddy.hernandez@fandm.edu>
2018-10-31 01:00:16 -0400

    added footer information + finished block content layout

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var h1 = document.getElementsByTagName('h1');
 
 function viewSale(h1) {
-  h1.innerHTML = 'ENJOY 15% OFF YOUR FIRST ORDER!' + '<br>' + 'CONTACT US FOR DETAILS';
+  h1.innerHTML = 'FREE SHIPPING ON 100+' + '<br>' + 'CONTACT US FOR MORE DETAILS';
   h1.style.color = 'rgba(58, 77, 177, 0.9)';
 }
